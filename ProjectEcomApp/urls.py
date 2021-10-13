cc210bb3389e8ef9fe5247d3972892b849c9a70b
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-10-13 11:13:37 -0300

    Configuracion app servicios

--- ProjectEcomApp/urls.py
+++ ProjectEcomApp/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.inicio, name="inicio"),
-    path('servicios/', views.servicios, name="servicios"),
     path('tienda/', views.tienda, name="tienda"),
     path('info/', views.info, name="info"),
     path('contacto/', views.contacto, name="contacto"),
--- ProjectEcomApp/views.py
+++ ProjectEcomApp/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render, HttpResponse
-from servicios.models import Servicio
 
 # Create your views here.
 def inicio(request):
 
     return render(request, 'ProjectEcomApp/inicio.html')
-def servicios(request):
-    servicios=Servicio.objects.all()
-    return render(request, 'ProjectEcomApp/servicios.html', {"servicios": servicios})
+
 def tienda(request):
 
     return render(request, 'ProjectEcomApp/tienda.html')
