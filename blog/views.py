ed20095d856fcf7d5bf46acdce564c3f71829ef4
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-10-18 13:05:18 -0300

    Filtrado de Noticias por categorias

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from blog.models import Noticias
-
+from blog.models import Noticias,Categoria
 # Create your views here.
 def blog(request):
     noticias=Noticias.objects.all()
     return render(request, 'blog/blog.html', {"noticias":noticias})
+
+def categoria(request, categoria_id):
+    #Listar post correspondientes a categoria
+    categoria=Categoria.objects.get(id=categoria_id)
+    #Mostrar los post 
+    noticias=Noticias.objects.filter(categorias=categoria)
+    return render(request, "blog/categorias.html", {'categoria': categoria, "noticias": noticias})
