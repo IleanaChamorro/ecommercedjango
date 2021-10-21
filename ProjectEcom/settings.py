2f80a76fa77f82c50afaa5a1aa78abded8474d01
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-10-21 10:01:38 -0300

    Estilos formulario

--- ProjectEcom/settings.py
+++ ProjectEcom/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'ProjectEcomApp',
     'servicios',
     'blog',
@@ -122,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
--- contacto/forms.py
+++ contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label="Tu Nombre", max_length=100)
     email=forms.CharField(label="Email", required=True)
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido", widget=forms.Textarea)
