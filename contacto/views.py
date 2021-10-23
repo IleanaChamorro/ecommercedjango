b06eab064db2620253ff1097ca5d7175fd4d4b9b
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-10-23 10:28:56 -0300

    Recuperacion datos enviados formulario

--- contacto/views.py
+++ contacto/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
 
 # Create your views here.
 def contacto(request):
     formulario_contacto=FormularioContacto()
+
+    #Recuperacion datos formulario
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        #Si se han rellenado todos los campos
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+            
+            return redirect("/contacto/?valido")
+
+
     return render(request, 'contacto/contacto.html', {'formulario': formulario_contacto})
